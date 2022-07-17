0d634436797534cbcea5379a9480503e57a64160
LiWenJu <2691162280@qq.com>
2022-07-17 16:45:08 +0800

    添加about界面的倒计时

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,66 +1,18 @@
-// pages/about/about.js
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-
+        day: "",
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
-    onLoad(options) {
-
+    call: function() {
+        let today=new Date();
+        let endtime=new Date('2022-9-9 00:00:00');
+        let res=endtime-today;
+        let day=parseInt(res/1000/60/60/24);
+        this.setData({
+            day
+        })
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide() {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload() {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh() {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom() {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage() {
-
+    onLoad: function(opitons) {
+        setInterval(this.call,1000)
     }
 })
+
